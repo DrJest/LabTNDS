dc610a1d8603fefc196e44d46c37b4efd3afaa81
Dr.Jest <dc.jest@gmail.com>
2014-01-17 11:05:49 +0100

    First Commit

--- Lezione_5/esercizio5.2.cpp
+++ Lezione_5/esercizio5.2.cpp
@@ -7,27 +7,24 @@
 #include "Bisezione.hpp"
 using namespace std;
 int main(int argc,char** argv){
-//controlla gli argomenti!!!
-if (argc!=4) {std::cerr<<"usage:"<<argv[0]<<"-estremo a- -estremo b- -precisione-"<<endl;
-return -1;
+  if (argc!=4) {std::cerr<<"usage:"<<argv[0]<<"-estremo a- -estremo b- -precisione-"<<endl;
+  return -1;
+  }
+  double a=atof(argv[1]);
+  double b=atof(argv[2]);
+  double prec=atof(argv[3]);
+  if(a>b){
+   double c=a;
+   a=b;
+   b=c;
+  }
+  Parabola p(3,5,-2);
+  Retta r(1, 1);
+
+  Bisezione bis(3000);
+  bis.setPrecision(prec);
+  double zero=bis.CercaZeri(a,b,&p);
+
+  std::cout << "Zero in: " << zero << std::endl;
+  return 0;
 }
-double a=atof(argv[1]);
-double b=atof(argv[2]);
-double prec=atof(argv[3]);
-if(a>b){
- double c=a;
- a=b;
- b=c;
-}
-Parabola p(3,5,-2);
-Retta r(1, 1);
-
-Bisezione bis(3000);
-bis.setPrecision(prec);
-double zero=bis.CercaZeri(a,b,&p);
-
-std::cout << "Zero in: " << zero << std::endl;
-return 0;
-
-}//incolla funzione scambia eventualmente
-
